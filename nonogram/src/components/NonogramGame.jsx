611c87078b77ebd3d7dd3996c2450603a3784617
Marshall-Artz <marshallarts12@gmail.com>
2022-09-28 17:23:29 -0500

    progress

--- nonogram/src/components/NonogramGame.jsx
+++ nonogram/src/components/NonogramGame.jsx
@@ -2,23 +2,30 @@ import { Button } from '@mui/material';
 
 const NonogramGame = () => {
   // const [style, setStyle] = useState("contained");
-  const grid = Array(25).fill({style: "contained", value: 0}).map((square) => ({...square, id: i++}));
+  const grid = Array(25).fill({sty: "outlined", val: 0});
 
-  function changeStyle(element) {
-    if (element.value === 0) {
-      element.value = 1;
-      element.style = "outlined";
-    } else if (element.value === 1) {
-      element.value = 0;
-      element.style = "contained";
+  function change(objElement) {
+    console.log("Before: ", objElement);
+    if (objElement.val === 0) {
+      objElement.val = 1;
+      objElement.sty = "outlined";
+    } else if (objElement.val === 1) {
+      objElement.val = 0;
+      objElement.sty = "contained";
     }
-
-    return element;
+    console.log("After: ", objElement);
   }
 
-  const buttonMap = grid.map((element) =>
-      <Button sx={{margin: "5px"}} onClick={(e) => {changeStyle(element); console.log(element.style)}} variant={element.style}/>
-  );
+  function checkSty(objElement) {
+    return objElement.sty;
+  }
+
+  const buttonMap = grid.map((objElement) => {
+    console.log(objElement);
+    return (
+      <Button sx={{margin: "3px", height: 60}} onClick={() => change(objElement)} variant={objElement.sty}/>
+    );
+  });
 
   return (
     <div>
