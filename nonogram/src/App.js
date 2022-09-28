ab5efe26451b1c7d54f8105257b3cbe30d4441d8
Marshall-Artz <marshallarts12@gmail.com>
2022-09-27 20:05:19 -0500

    simplifying code, not working

--- nonogram/src/App.js
+++ nonogram/src/App.js
@@ -1,72 +1,12 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React from 'react';
+import NonogramGame from './components/NonogramGame';
 
 function App() {
-  const [squares, setSquares] = useState([])
-
-  const createGrid = () => {
-    var i = 0
-    const grid = Array(25).fill({"state" : ""}) 
-      .map((square) => ({...square, id: i++}))
-    setSquares(grid)
-    console.log(squares)
-  }
-
-  const changeGrid = (id, state) => {
-    const grid = [...squares]
-    for(var j = 0; j < squares.length; j++) {
-      if(grid[j].id === id) {
-        grid[j].state = state
-        console.log(grid[j].id, grid[j].state)
-        break;
-      }
-    }
-    setSquares(grid)
-  }
 
   return (
     <div className="App">
-      <button className = "create" onClick = {createGrid}>CREATE!</button>
-      
-      <div className = "square-grid">
-          {squares.map(square => (
-            <div className = "square" key = {square.id}>
-              <div className={square.state}>
-                <img
-                  className="default" src="/img/default.png" alt = "default square" draggable="false" 
-                  onClick={(e) => {
-                    changeGrid(square.id, "clickedShow")
-                  }}
-                  onContextMenu={(e) => {
-                    e.preventDefault()
-                    changeGrid(square.id, "xShow")
-                  }}
-                />
-                <img
-                  className="clicked" src="/img/clicked.png" alt = "clicked square" draggable="false"
-                  onClick={(e) => {
-                    changeGrid(square.id, "")
-                  }}
-                  onContextMenu={(e) => {
-                    e.preventDefault()
-                    changeGrid(square.id, "xShow")
-                  }}
-                />
-                
-                <img 
-                  className="x" src="/img/x.png" alt = "x square" draggable="false"
-                  onClick={(e) => {
-                    changeGrid(square.id, "clickedShow")
-                  }}
-                  onContextMenu={(e) => {
-                    e.preventDefault()
-                    changeGrid(square.id, "")
-                  }}
-                />
-              </div>
-            </div>
-          ))}
-        </div>
+      <NonogramGame></NonogramGame>
     </div>
   );
 }
