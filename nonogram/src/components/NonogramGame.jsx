5198246450ffeaba9598fa8dc2a97e27ddf82ef2
Marshall-Artz <marshallarts12@gmail.com>
2022-09-29 15:37:12 -0500

    still not working, but very close

--- nonogram/src/components/NonogramGame.jsx
+++ nonogram/src/components/NonogramGame.jsx
@@ -4,26 +4,25 @@ const NonogramGame = () => {
   // const [style, setStyle] = useState("contained");
   const grid = Array(25).fill({sty: "outlined", val: 0});
 
-  function change(objElement) {
+  function change(e, objElement) {
+    console.log("Event: ", e);
     console.log("Before: ", objElement);
     if (objElement.val === 0) {
       objElement.val = 1;
-      objElement.sty = "outlined";
+      objElement.sty = "contained";
     } else if (objElement.val === 1) {
       objElement.val = 0;
-      objElement.sty = "contained";
+      objElement.sty = "outlined";
     }
     console.log("After: ", objElement);
   }
 
-  function checkSty(objElement) {
-    return objElement.sty;
-  }
+  // onClick={() => change(objElement)}
 
   const buttonMap = grid.map((objElement) => {
-    console.log(objElement);
+    console.log(objElement.sty);
     return (
-      <Button sx={{margin: "3px", height: 60}} onClick={() => change(objElement)} variant={objElement.sty}/>
+      <Button sx={{margin: "3px", height: 60}} onClick={(e) => change(e, objElement)} variant={objElement.sty} />
     );
   });
 
