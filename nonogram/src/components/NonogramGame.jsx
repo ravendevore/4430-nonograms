231cb079458462c05bce5e893e021795d99575fb
collin-lyon <collinlyon@my.unt.edu>
2022-10-01 09:12:06 -0500

    Added row and column headers, with proper format

--- nonogram/src/components/NonogramGame.jsx
+++ nonogram/src/components/NonogramGame.jsx
@@ -5,6 +5,8 @@ import React, {useState} from 'react';
 const NonogramGame = () => {
   var i = 0;
   const [grid, setGrid] = useState(Array(25).fill({sty: "outlined", val: 0}).map((obj) => ({...obj, key: i++})))
+  const [colH, setColH] = useState(Array(5).fill({val: "12"}).map((obj) => ({...obj, key: i++})))
+  const [rowH, setRowH] = useState(Array(5).fill({val: "21"}).map((obj) => ({...obj, key: i++})))
   //this is how we can set the original state outside of a function, by passing it to the useState function
   //also, .map gives each object a key, which is needed to give each button a unique key
 
@@ -27,7 +29,7 @@ const NonogramGame = () => {
     console.log(objElement.sty);
     return (
       <Button
-        sx={{margin: "3px", height: 60}}
+        sx={{borderRadius: "0px", margin: "0px", border: "1px dotted white", height: "100%", width: "100%"}}
         key = {objElement.key}
         onClick={(e) => {change(e, objElement); setGrid([...grid])}}
         variant={objElement.sty}
@@ -35,9 +37,39 @@ const NonogramGame = () => {
     );
   });
 
+  const colHMap = colH.map((objElement) => {
+    return (
+      <div className="textCol"
+        key = {objElement.key}
+      >
+        {objElement.val}
+      </div>
+    );
+  });
+
+  const rowHMap = rowH.map((objElement) => {
+    return (
+      <div className="textRow"
+        key = {objElement.key}
+      >
+        {objElement.val}
+      </div>
+    );
+  });
+
   return (
-    <div>
-      {buttonMap}
+    <div className="grid">
+      {[
+      <div className="gridColH" key="0">
+        {colHMap}
+      </div>,
+      <div className="gridRowH" key="1">
+        {rowHMap}
+      </div>,
+      <div className="gridCont" key="2">
+        {buttonMap}
+      </div>
+    ]}
     </div>
   );
 }
