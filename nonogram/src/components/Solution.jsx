c2ca62db16dd0b8f8a028ee4f63899308a82e3f5
Raven Devore <ravendevore@my.unt.edu>
2022-10-08 19:40:49 -0500

    Re-added function to make row/col nums

--- nonogram/src/components/Solution.jsx
+++ nonogram/src/components/Solution.jsx
@@ -17,7 +17,8 @@ const SolutionButton = () => {
     0, 0, 0, 0, 0
   ]
 
-  function checkSolution() {
+  function checkSolution() { // FIXME - needs to get grid
+    console.log("HERE")
     let correct = true
     for(var j = 0; j < soln.length; j++) {
       if(
@@ -30,10 +31,40 @@ const SolutionButton = () => {
     alert(correct ? "Solution is correct!" : "Solution is not correct...")
   }
 
+  const solnDim = 5 // FIXME - generate from solution array?
+  function genNums(isRow) {
+    let fullList = []
+    for (let i = 0; i < solnDim; i++) {
+      let currList = []
+      let blockSize = 0
+      let isBlock = false
+      for (let j = 0; j < solnDim; j++) {
+        let cellNum = isRow ? i*solnDim+j : j*solnDim+i // Currently works with 1D array
+        if (soln[cellNum] === 1) {
+          isBlock = true
+          blockSize++
+        }
+        else if (isBlock) {
+          currList.push(blockSize)
+          blockSize = 0
+          isBlock = false
+        }
+      }
+      if (blockSize != 0) {
+        currList.push(blockSize)
+      }
+      if (currList.length === 0) {
+        currList.push(0)
+      }
+      fullList.push(currList)
+    }
+    console.log(fullList) // FIXME - need to display next to grid
+  }
+
   return (
     <div className="solnButton" key="0">
       <Button
-        onClick={checkSolution}
+        onClick={() => checkSolution()}
         variant = "contained"
       >
       Check Solution
