eb0063878cf9abf4618e84117354f6c7677a474c
Raven Devore <ravendevore@my.unt.edu>
2022-10-12 18:33:04 -0500

    Added red color for X

--- nonogram/src/components/NonogramGame.jsx
+++ nonogram/src/components/NonogramGame.jsx
@@ -7,18 +7,49 @@ import React, {useState} from 'react';
 const NonogramGame = () => {
   var i = 0;
   const [grid, setGrid] = useState(Array(25).fill({sty: "outlined", val: 0}).map((obj) => ({...obj, key: i++})))
-  const [colH, setColH] = useState(Array(5).fill({val: "12"}).map((obj) => ({...obj, key: i++})))
-  const [rowH, setRowH] = useState(Array(5).fill({val: "21"}).map((obj) => ({...obj, key: i++})))
-  //this is how we can set the original state outside of a function, by passing it to the useState function
-  //also, .map gives each object a key, which is needed to give each button a unique key
+  const soln = [
+    1, 1, 1, 1, 1,
+    1, 0, 1, 1, 1,
+    0, 0, 1, 1, 0,
+    1, 0, 1, 0, 1,
+    0, 0, 0, 0, 0
+  ]
 
-  function change(e, objElement) {
+  const [colH, setColH] = useState(genNums(0).map((obj) => ({val: obj, key: i++})))
+  const [rowH, setRowH] = useState(genNums(1).map((obj) => ({val: obj, key: i++})))
+
+  function arrToObj(array) {
+    let output = []
+    for(let j = 0; j < array.length; j++) {
+        output[j] = {"val": array[j]}
+    }
+    console.log(output);
+    return output;
+  }
+
+  function change(e, objElement) { // left click - toggle fill
     console.log("Event: ", e);
     console.log("Before: ", objElement);
-    if (objElement.val === 0) {
+    if (objElement.val === 0 || objElement.val === 2) { //b lank or X: change to filled
       objElement.val = 1;
       objElement.sty = "contained";
-    } else if (objElement.val === 1) {
+      objElement.color = "primary"; // blue for fill
+    } else if (objElement.val === 1) { // filled: change to blank
+      objElement.val = 0;
+      objElement.sty = "outlined";
+    }
+    console.log("After: ", objElement);
+  }
+
+  function changeRight(e, objElement) { // right click - toggle X
+    e.preventDefault() // prevent context menu from showing
+    console.log("Event: ", e);
+    console.log("Before: ", objElement);
+    if (objElement.val === 0 || objElement.val === 1) { // currently blank or filled: change to X
+      objElement.val = 2;
+      objElement.sty = "contained";
+      objElement.color = "error"; // red for X - FIXME need image
+    } else if (objElement.val === 2) { // currently X: change to blank
       objElement.val = 0;
       objElement.sty = "outlined";
     }
@@ -34,7 +65,9 @@ const NonogramGame = () => {
         sx={{borderRadius: "0px", margin: "0px", border: "1px dotted white", height: "100%", width: "100%"}}
         key = {objElement.key}
         onClick={(e) => {change(e, objElement); setGrid([...grid])}}
+        onContextMenu={(e) => {changeRight(e, objElement); setGrid([...grid])}}
         variant={objElement.sty}
+        color={objElement.color}
       >
         <CloseIcon fontSize="large" sx={visuallyHidden} />
       </Button>
@@ -61,20 +94,71 @@ const NonogramGame = () => {
     );
   });
 
+  function checkSolution() {
+    let correct = true
+    for(var j = 0; j < soln.length; j++) {
+      if(
+        (soln[j] === 1 && grid[j].val !== 1) || 
+        (soln[j] === 0 && grid[j].val === 1)
+      ) {
+        correct = false
+      }
+    }
+    alert(correct ? "Solution is correct!" : "Solution is not correct...") 
+  }
+
+
+  function genNums(isRow) {
+    const solnDim = 5
+    let fullList = []
+    for (let i = 0; i < solnDim; i++) {
+      let currList = []
+      let blockSize = 0
+      let isBlock = false
+      for (let j = 0; j < solnDim; j++) {
+        let cellNum = isRow ? i*solnDim+j : j*solnDim+i // Currently works with 1D array
+        if (soln[cellNum] === 1) {
+          isBlock = true
+          blockSize++
+        }
+        else if (isBlock) {
+          currList.push(blockSize)
+          blockSize = 0
+          isBlock = false
+        }
+      }
+      if (blockSize != 0) {
+        currList.push(blockSize)
+      }
+      if (currList.length === 0) {
+        currList.push(0)
+      }
+      fullList.push(currList)
+    }
+    return fullList;
+  }
+
   return (
-    <div className="grid">
-      {[
-      <div className="gridColH" key="0">
-        {colHMap}
-      </div>,
-      <div className="gridRowH" key="1">
-        {rowHMap}
-      </div>,
-      <div className="gridCont" key="2">
-        {buttonMap}
-      </div>
-    ]}
-    </div>
+    <React.Fragment>
+        <div className="grid">
+            <div className="gridColH">
+                {colHMap}
+            </div>
+            <div className="gridRowH">
+                {rowHMap}
+            </div>
+            <div className="gridCont">
+                {buttonMap}
+            </div>
+        </div>
+        <Button className="solnButton"
+            onClick={() => checkSolution()}
+            variant = "contained"
+        >
+            Check Solution
+        </Button>
+    </React.Fragment>
+    
   );
 }
 
