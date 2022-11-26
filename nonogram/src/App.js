92d372d927b69d081d731e89c7e570851604c403
Raven Devore <ravendevore@my.unt.edu>
2022-11-26 17:55:56 -0600

    Added /create page to create puzzles

--- nonogram/src/App.js
+++ nonogram/src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
 import React from 'react';
 import NonogramGame from './components/NonogramGame';
+import CreatePuzzle from './components/CreatePuzzle'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
-      <NonogramGame></NonogramGame>
+      <Routes>
+        <Route path="/" element={<NonogramGame />}/>
+        <Route path="/create" element={<CreatePuzzle />}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+
+// import './App.css';
+// import React from 'react';
+// import NonogramGame from './components/NonogramGame';
+
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <NonogramGame></NonogramGame>
+//     </div>
+//   );
+// }
+
+// export default App;
