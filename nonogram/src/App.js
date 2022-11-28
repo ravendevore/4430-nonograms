f38fae6b3097c10e2333c50d165ddc21ec1179bb
Raven Devore <ravendevore@my.unt.edu>
2022-11-27 22:09:03 -0600

    Navbar now highlights link for current page

--- nonogram/src/App.js
+++ nonogram/src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import React from 'react';
 import NonogramGame from './components/NonogramGame';
 import CreatePuzzle from './components/CreatePuzzle';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
       <ul className="navbar">
-        <li className="navItem">
-          <Link style={{textDecoration: "none"}} to="/">Solve a Random Puzzle</Link>
+        <li>
+          <NavLink className={({isActive}) => isActive ? "navLink navActive": "navLink" } to="/">Solve a Random Puzzle</NavLink>
         </li>
-        <li className="navItem">
-          <Link style={{textDecoration: "none"}} to="/create">Create Your Own</Link>
+        <li>
+          <NavLink className={({isActive}) => isActive ? "navLink navActive": "navLink" } to="/create">Create Your Own</NavLink>
         </li>
       </ul>
       <Routes>
