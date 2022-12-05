66e07b96571da55f44f1bf0e398ae19657e584aa
Raven Devore <ravendevore@my.unt.edu>
2022-12-04 21:35:43 -0600

    Added dropdown to switch puzzle size

--- nonogram/src/App.js
+++ nonogram/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import NonogramGame from './components/NonogramGame';
 import CreatePuzzle from './components/CreatePuzzle';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink, Link } from 'react-router-dom';
 
 function App() {
 
@@ -10,10 +10,24 @@ function App() {
     <div className="App">
       <ul className="navbar">
         <li>
-          <NavLink className={({isActive}) => isActive ? "navSolve navActive": "navSolve" } to="/">Solve a Random Puzzle</NavLink>
+          <div className="dropdownSolve">
+            <NavLink className={({isActive}) => isActive ? "navSolve navActive": "navSolve" } to="/">Solve a Random Puzzle</NavLink>
+            <div className="dropdownSolveContent">
+              <a href="/?dim=5" className="solveLink" >5 x 5</a>
+              <a href="/?dim=10" className="solveLink" >10 x 10</a>
+              <a href="/?dim=15" className="solveLink" >15 x 15</a>
+            </div>
+          </div>
         </li>
         <li>
-          <NavLink className={({isActive}) => isActive ? "navCreate navActive": "navCreate" } to="/create">Create Your Own</NavLink>
+          <div className="dropdownCreate">
+            <NavLink className={({isActive}) => isActive ? "navCreate navActive": "navCreate" } to="/create">Create Your Own</NavLink>
+            <div className="dropdownCreateContent">
+              <a href="/create?dim=5" className="solveLink" >5 x 5</a>
+              <a href="/create?dim=10" className="solveLink" >10 x 10</a>
+              <a href="/create?dim=15" className="solveLink" >15 x 15</a>
+            </div>
+          </div>
         </li>
       </ul>
       <Routes>
@@ -25,3 +39,23 @@ function App() {
 }
 
 export default App;
+
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <ul className="navbar">
+//         <li>
+//           <NavLink className={({isActive}) => isActive ? "navSolve navActive": "navSolve" } to="/">Solve a Random Puzzle</NavLink>
+//         </li>
+//         <li>
+//           <NavLink className={({isActive}) => isActive ? "navCreate navActive": "navCreate" } to="/create">Create Your Own</NavLink>
+//         </li>
+//       </ul>
+//       <Routes>
+//         <Route path="/" element={<NonogramGame />}/>
+//         <Route path="/create" element={<CreatePuzzle />}/>
+//       </Routes>
+//     </div>
+//   );
+// }
